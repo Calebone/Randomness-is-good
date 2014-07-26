68bfb9b8ee5e8f9cb5e195f29c7c3ea4b7446116
Calebone <calebone@att.net>
2014-07-26 16:09:25 -0500

    fixed for loop

--- Array/src/App.java
+++ Array/src/App.java
@@ -1,12 +1,22 @@
-
 public class App {
 
 	public static void main(String[] args) {
-		String[] fruits = {"apples", "banana", "pear", "rainbow brust"};
-		
-		for(String fruit: fruits) {
+		String[] fruits = { "apples", "banana", "pear", "rainbow brust" };
+
+		for (String fruit : fruits) {
 			System.out.println(fruit);
+			
 		}
+			
+			String newshire = "lossy";
+			
+			String text = "null";
+			
+			String[] groups = {text, "fruits", newshire};
+			for (String group : groups) {
+		System.out.println(group);
+		
 		}
 	}
+}
 
